22fc1ac22711ab0e2e719c5c6b885fe6908f7f84
William Luqui <william.luqui@gmail.com>
2022-10-14 15:26:38 -0300

    integração mercado pago

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 8080 || 3000
+const port = 3000
 const MercadoPago = require("mercadopago")
 const {TOKEN_TEST} = process.env;
 
